bb62c9b205848bc4019578cb34307a87152258b0
eccentric <eccentric0521@gmail.com>
2023-05-16 10:16:41 +0800

    sout 1

--- Tranding-Platform/src/main/java/com/eccentric/tranding/controller/RoleController.java
+++ Tranding-Platform/src/main/java/com/eccentric/tranding/controller/RoleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
  * @author eccentric
  */
 @RestController
-@RequestMapping("/role")
+@RequestMapping(value = "/role",method = RequestMethod.GET)
 public class RoleController extends BaseController{
 
     @Autowired
